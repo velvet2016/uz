651f4096c07ad4a64c6b9ad491b9361093358d60
velvet2016 <astudzinsky@gmail.com>
2019-03-03 12:42:45 +0200

    fixes

--- src/main/java/pages/PageObject.java
+++ src/main/java/pages/PageObject.java
@@ -2,6 +2,7 @@ package pages;
 
 import config.Config;
 //import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,52 +11,53 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-public abstract class PageObject{
+public abstract class PageObject {
     protected WebDriver driver;
 
-    public PageObject(WebDriver driver){
-        System.setProperty("webdriver.gecko.driver","/home/linux/programs/geckodriver");
-        //logger.info("loading page: " + this.getClass().getSimpleName());
+    public PageObject(WebDriver driver) {
+        logger.info("loading page: " + this.getClass().getSimpleName());
         this.driver = driver;
         PageFactory.initElements(driver, this);
-        // logger.info("chekcing page title");
+        logger.info("chekcing page title");
         String actualTitle = driver.getTitle();
-        if (isCheckTitleNeeded() && !actualTitle.equals(getExpectedTitle())){
-            throw new IllegalStateException("Page was not loaded. Expected page title "+ getExpectedTitle()+"but actual: "+actualTitle);
+        if (isCheckTitleNeeded() && !actualTitle.equals(getExpectedTitle())) {
+            throw new IllegalStateException("Page was not loaded. Expected page title " + getExpectedTitle() + "but actual: " + actualTitle);
         }
-        // logger.info("chekcing page title: Success");
-        //logger.info("Page loaded: " + this.getClass().getSimpleName());
+        logger.info("chekcing page title: Success");
+        logger.info("Page loaded: " + this.getClass().getSimpleName());
     }
-    abstract protected  void isLoaded();
 
-    //private static final Logger logger = Logger.getLogger(this.getClass());
+    abstract protected void isLoaded();
+
+    private final Logger logger = Logger.getLogger(this.getClass());
 
 
-    protected void waitForPresense(By by) throws Error{
-        //     logger.debug("waiting till element " + by.toString() + " loaded");
+    protected void waitForPresense(By by) throws Error {
+        logger.debug("waiting till element " + by.toString() + " loaded");
         new WebDriverWait(driver, Config.EXPLICIT_WAIT_SMALL).until(ExpectedConditions.visibilityOfElementLocated(by));
     }
 
     private void waitForVisibilityWithSmallWait(WebElement element) {
         int secondsToWait = Config.EXPLICIT_WAIT_SMALL;
         String message = "waiting till element " + element.toString() + " become visible. Explicit wait = " + secondsToWait;
-        //    logger.debug(message);
+        logger.debug(message);
         new WebDriverWait(driver, secondsToWait).until(ExpectedConditions.visibilityOf(element));
-        //    logger.debug(message+"...Done");
+        logger.debug(message + "...Done");
     }
 
 
-    protected void logInputAction(String inputFieldName, String inputValue){
-        //logger.info("typing \""+inputValue+"\" to  \""+ inputFieldName+"\"");
+    protected void logInputAction(String inputFieldName, String inputValue) {
+        logger.info("typing \"" + inputValue + "\" to  \"" + inputFieldName + "\"");
     }
 
-    protected void logClick(WebElement element){
-        // logger.info("clicking  on "+"\""+ element.toString()+"\"...");
+    protected void logClick(WebElement element) {
+        logger.info("clicking  on " + "\"" + element.toString() + "\"...");
         element.click();
     }
+
     protected abstract String getExpectedTitle();
 
-    protected  boolean isCheckTitleNeeded() {
+    protected boolean isCheckTitleNeeded() {
         return true;
     }
 }
